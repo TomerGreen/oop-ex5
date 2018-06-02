046becd5450647272c50753819cd9185c1f5d590
ADVA\Adva <advamadvil@gmail.com>
2018-06-02 15:09:50 +0300

    mj

---
+++ src/order/AbsOrder.java
@@ -0,0 +1,12 @@
+import java.io.File;
+
+/**
+ *  A subclass of the Order superclass, compare between files based on the ABC order of their AbsolutePath.
+ */
+public class AbsOrder extends Order {
+
+	@Override
+	public int compare(File o1, File o2) {
+		return o1.getAbsolutePath().compareTo(o2.getAbsolutePath());
+	}
+}
---
+++ src/order/ReverseOrder.java
@@ -0,0 +1,21 @@
+import java.io.File;
+
+/**
+ * A subclass of the Order superclass, compare between files based on the opposite of the determined order.
+ */
+public class ReverseOrder extends Order {
+
+	private static final int REVERSE_FACTOR = -1;
+
+	private Order order;
+
+	//constructor
+	public ReverseOrder(Order order) {
+		this.order = order;
+	}
+
+	@Override
+	public int compare(File o1, File o2) {
+		return order.compare(o1,o2)*REVERSE_FACTOR;
+	}
+}
